aa2fb8c3c541a68d4252426de86f015a68d28f1f
ncanzonieri <ncanzonieri@itba.edu.ar>
2025-05-04 20:14:31 -0300

    seteando el nuevo entorno

--- x64BareBones-master/run.sh
+++ x64BareBones-master/run.sh
@@ -1,4 +1,6 @@
-if [ "$1" = "-d" ]; then
+#!/bin/bash
+if [ "$1" = "-d" ]
+then
     qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
 else
     qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
