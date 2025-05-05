13dd92298f875db380722bf7563c8b3aa1f73614
ncanzonieri <ncanzonieri@itba.edu.ar>
2025-05-04 23:13:45 -0300

    sacando el audio

--- x64BareBones-master/run.sh
+++ x64BareBones-master/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$1" = "-d" ]
 then
-    qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+    qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512
 else
-    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
 fi
