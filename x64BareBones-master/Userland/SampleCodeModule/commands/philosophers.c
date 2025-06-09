76b727c6b86dc62d2e9e1b7c462873ebdfe2d43a
ncanzonieri <ncanzonieri@itba.edu.ar>
2025-06-09 16:30:42 -0300

    mejorando a filo uba

--- x64BareBones-master/Userland/SampleCodeModule/commands/philosophers.c
+++ x64BareBones-master/Userland/SampleCodeModule/commands/philosophers.c
@@ -34,7 +34,7 @@ int phylo(int argc, char **argv) {
     printInstructions();
 
     for (int i = 0; i < MIN_PHILO; i++) {
-        addPhilo();
+        addPhilo(i);
     }
 
     int running = 1;
@@ -55,19 +55,19 @@ int phylo(int argc, char **argv) {
 void canEat(int i) {
     int left = (i + philoCount - 1) % philoCount;
     int right = (i + 1) % philoCount;
-    if(philosophers[left].state != EATING && philosophers[right].state != EATING){
+    if(philosophers[i].state == HUNGRY && philosophers[left].state != EATING && philosophers[right].state != EATING){
         philosophers[i].state = EATING;
         sys_semPost(forkNames[i]);
-        
+        printStatus();
     }
 }
 
 void think() {
-    sys_sleep(1000);
+    sys_sleep(5);
 }
 
 void eat() {
-    sys_sleep(2000);
+    sys_sleep(10);
 }
 
 void takeForks(int i) {
@@ -104,49 +104,52 @@ int philosopher(int argc, char **argv) {
     return 0;
 }
 
-void addPhilo() {
+void addPhilo(int i) {
+    
     if (philoCount >= MAX_PHILO) {
         printf("Maximo de filosofos alcanzado, no se puede agregar mas.\n");
         return;
     }
     sys_semWait(mutex);
-    if (philoCount < MAX_PHILO) {
-        int i = philoCount;
-        philosophers[i].state = THINKING;
+    philosophers[i].state = THINKING;
 
-        sys_semOpen(forkNames[i], 1);
+    sys_semOpen(forkNames[i], 1);
 
-        char philoNumber[4];
-        itoa(i, philoNumber);
-        char *argv[] = {philoNumber, 0};
+    char philoNumber[4];
+    itoa(i, philoNumber);
+    char *argv[] = {philoNumber, 0};
 
-        int fds[2] = {0, 1};
-        philosophers[i].pid = sys_createProcess("philosopher", 1, 0, (ProcessEntry)&philosopher, argv, 1, fds);
-
-        philoCount++;
-        printf("Filosofo %d creado\n", philoCount);
-        printStatus();
+    int fds[2] = {0, 1};
+    philosophers[i].pid = sys_createProcess("philosopher", 1, 0, (ProcessEntry)&philosopher, argv, 1, fds);
+    if(philosophers[i].pid < 0) {
+        printf("Error al crear el filosofo %d\n", i);
+        sys_semPost(mutex);
+        return;
     }
+    philoCount++;
+    printf("Filosofo %d creado\n", philoCount);
+    printStatus();
     sys_semPost(mutex);
 }
 
-void removePhilo() {
+void removePhilo(int i) {
+    
     if (philoCount <= MIN_PHILO) {
         printf("No se puede eliminar mas filosofos, minimo alcanzado.\n");
         return;
     }
-    philoCount--;
-    int left = 0;
-    int right = philoCount - 1;
     sys_semWait(mutex);
+    int right = (i + 1) % philoCount;
+    int left = (i + philoCount - 1) % philoCount;
     while(philosophers[left].state == EATING && philosophers[right].state == EATING) {
         sys_semPost(mutex);
-        sys_semWait(forkNames[philoCount]);
+        sys_semWait(forkNames[i]);
         sys_semWait(mutex);
     }
-    sys_semClose(forkNames[philoCount]);
-    sys_killProcess(philosophers[philoCount].pid);
-    printf("Rip filosofo %d\n", philoCount + 1);
+    sys_semClose(forkNames[i]);
+    sys_killProcess(philosophers[i].pid);
+    printf("Rip filosofo %d\n", philoCount);
+    philoCount--;
     printStatus();
     sys_semPost(mutex);
 }
@@ -158,7 +161,7 @@ void printInstructions() {
     printf("Press 'a' to add a philosopher\n");
     printf("Press 'r' to remove a philosopher\n");
     printf("Press 'e' to exit\n");
-
+    printf("Press 'c' to clear the screen\n");
     printf("Press 's' to start\n");
     char c = 0;
     while (c != 's') {
@@ -175,11 +178,11 @@ void printInstructions() {
 
 int processInput(char c) {
     if (c == 'a') {
-        addPhilo();
+        addPhilo(philoCount);
         return 1;
     }
     if (c == 'r') {
-        removePhilo();
+        removePhilo(philoCount - 1);
         return 1;
     }
     if(c == 'c'){
